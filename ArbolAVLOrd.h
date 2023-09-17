c36ecc194a073ad8cf62adf5c74553b695b0ca8c
niko2307 <142281335+niko2307@users.noreply.github.com>
2023-09-17 17:56:33 -0500

    Update ArbolAVLOrd.h

--- ArbolAVLOrd.h
+++ ArbolAVLOrd.h
@@ -27,17 +27,21 @@ public:
     int altura();
     int altura(NodoBin<T> *nodo);
     int tamahno();
+    int tamano(NodoBin<T> *nodo);
     bool insert(T &val); // modificar para que sea AVL
     bool erase(T &val);  // modificar para que sea AVL
     NodoBin<T> rotacionIzq(NodoBin<T> *nodo);
     NodoBin<T> rotacionDer(NodoBin<T> *nodo);
     int getBalanceFactor(NodoBin<T> *nodo);
     NodoBin<T> *eraseNode(T &val, NodoBin<T> *nodo);
-    NodoBin<T> nodeMinVal(NodoBin<T> *nodo);
     void preOrden();
+    void preOrden(NodoBin<T> *nodo);
     void inOrden();
+    void inOrden(NodoBin<T> *nodo);
     void posOrden();
+    void posOrden(NodoBin<T> *nodo);
     void nivelOrden();
+    
     NodoBin<T> *insertRec(NodoBin<T> *nodo, T valor);
     void inOrdenLista(std::list<T> &lista);
     void inOrdenListaRaiz(NodoAVL *nodo, std::list<T> &lista);
