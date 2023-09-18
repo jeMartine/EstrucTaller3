8af580b525ea3088c7cee6621fc511c1b0d54051
niko2307 <142281335+niko2307@users.noreply.github.com>
2023-09-17 21:20:06 -0500

    Create NodoAvl.h

---
+++ NodoAvl.h
@@ -0,0 +1,25 @@
+#ifndef NODOAVL_H
+#define NODOAVL_H
+template <class T>
+class NodoAVL
+{
+private:
+    T valor;
+    NodoAVL<T> *izquierdo;
+    NodoAVL<T> *derecho;
+    int altura;
+    public:
+    NodoAVL(T &val) : valor(val), izquierdo(nullptr), derecho(nullptr), altura(1) {}
+    T obtenerValor();
+    NodoAVL<T> *obtenerIzquierdo();
+    NodoAVL<T> *obtenerDerecho();
+    int obtenerAltura();
+    void fijarValor( T &val);
+    void fijarIzquierdo(NodoAVL<T> *nodo) ;
+    void fijarDerecho(NodoAVL<T> *nodo) ;
+    void fijarAltura(int alt);
+};
+#include "NodoAvl.hxx"
+
+
+#endif // NODOAVL_H
