67499e18a0a85437577ced99c57f2f69f039a4e6
niko2307 <142281335+niko2307@users.noreply.github.com>
2023-09-17 21:20:33 -0500

    Create NodoAvl.hxx

---
+++ NodoAvl.hxx
@@ -0,0 +1,35 @@
+#include "NodoAvl.h"
+
+template <class T>
+T NodoAVL<T>::obtenerValor(){
+    return valor;
+}
+template <class T>
+NodoAVL<T> * NodoAVL<T>::obtenerIzquierdo(){
+return izquierdo;
+}
+template <class T>
+NodoAVL<T> * NodoAVL<T>::obtenerDerecho(){
+return derecho;
+}
+
+template <class T>
+int NodoAVL<T>:: obtenerAltura(){
+return altura;
+}
+template <class T>
+void NodoAVL<T>::fijarValor( T &val){
+valor = val;
+}
+template <class T>
+void NodoAVL<T>::fijarIzquierdo(NodoAVL<T> *nodo){
+izquierdo = nodo;
+}
+template <class T>
+void NodoAVL<T>::fijarDerecho(NodoAVL<T> *nodo){
+derecho = nodo;
+}
+template <class T>
+void NodoAVL<T>::fijarAltura(int alt){
+altura = alt;
+}
