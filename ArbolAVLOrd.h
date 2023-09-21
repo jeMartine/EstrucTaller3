bf7ac63c7797219e21a232db912d53c6276c7144
niko2307 <142281335+niko2307@users.noreply.github.com>
2023-09-21 07:19:00 -0500

    Update ArbolAVLOrd.h

--- ArbolAVLOrd.h
+++ ArbolAVLOrd.h
@@ -15,17 +15,31 @@ public:
     ~ArbolAVLOrd();
     T datoRaiz();
     bool esVacio();
-    int altura();
-    int altura(NodoAVL<T> *nodo);
+    int size();
+    int size(NodoAVL<T> *nodo);
+    int calcHeight(NodoAVL<T> *nodo);
     int tamahno();
     int tamano(NodoAVL<T> *nodo);
     bool insert(T &val); // modificar para que sea AVL
     bool erase(T &val);  // modificar para que sea AVL
+
+    void updateHeight(NodoAVL<T> *nodo);
+    NodoAVL<T> *LLR(NodoAVL<T> *nodo);
+    NodoAVL<T> *RRR(NodoAVL<T> *nodo);
+    NodoAVL<T> *LRR(NodoAVL<T> *nodo);
+    NodoAVL<T> *RLR(NodoAVL<T> *nodo);
+    NodoAVL<T> *balanceee(NodoAVL<T> *nodo);
+
     NodoAVL<T> *rotacionIzq(NodoAVL<T> *nodo);
     NodoAVL<T> *rotacionDer(NodoAVL<T> *nodo);
     int getBalanceFactor(NodoAVL<T> *nodo);
-    NodoAVL<T> *insertRec(NodoAVL<T> *nodo, T valor);
+
+    // NodoAVL<T> *insertRec(NodoAVL<T> *nodo, T valor);
+    NodoAVL<T> *insertRec(NodoAVL<T> *nodo, NodoAVL<T> *parent, T valor);
+
     NodoAVL<T> *eraseNode(T &val, NodoAVL<T> *nodo);
+    int obtenerCantidadHijos(NodoAVL<T> *nodo);
+    NodoAVL<T> obtenerHijoNoVacio(NodoAVL<T> *nodo);
     void preOrden();
     void preOrden(NodoAVL<T> *nodo);
     void inOrden();
@@ -34,10 +48,14 @@ public:
     void posOrden(NodoAVL<T> *nodo);
     void nivelOrden();
 
-    void inOrdenLista(std::list<T> &lista);
+<<<<<<< Updated upstream
+   std::list<T>& inOrdenLista(std::list<T> &lista);
+=======
+    std::list<T> &inOrdenLista(std::list<T> &lista);
+>>>>>>> Stashed changes
     void inOrdenListaRaiz(NodoAVL<T> *nodo, std::list<T> &lista);
 };
 
 #include "ArbolAVLOrd.hxx"
 
-#endif // ARBOLAVL_H
+#endif // ARBOLAVL_H
